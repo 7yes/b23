5d45f831e65c5c3ff73bc21c3cfb9b2aec598537
mme <7yesid@gmail.com>
2023-07-13 03:15:58 -0500

    frags nav logic in mainactivity

--- app/src/main/java/com/example/myapplication/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activities/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.myapplication.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.ui.fragments.HomeFragment
+import com.example.myapplication.ui.fragments.ProfileFragment
+import com.example.myapplication.ui.fragments.SupportFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().add(R.id.frameContainer,HomeFragment()).commit()
+
+        binding.bottomNavMenu.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.mHome -> supportFragmentManager.beginTransaction().replace(R.id.frameContainer,HomeFragment()).commit()
+                R.id.mProfile -> supportFragmentManager.beginTransaction().replace(R.id.frameContainer,ProfileFragment()).commit()
+                R.id.mSupport -> supportFragmentManager.beginTransaction().replace(R.id.frameContainer,SupportFragment()).commit()
+                R.id.mExit -> finish()
+            }
+            true
+        }
     }
 }
