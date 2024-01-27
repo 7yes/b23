b157d0b626c9d7ed52da694b7f4eafc4437b2ce8
jesm <yesid_android@proton.me>
2024-01-26 21:28:37 -0600

    nothing

--- app/src/main/java/com/example/myapplication/model/DogsResponse.kt
+++ app/src/main/java/com/example/myapplication/model/DogsResponse.kt
@@ -4,5 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class DogsResponse(
     @SerializedName("message") val images: List<String>,
-    @SerializedName("status") val status: String
+    @SerializedName("status") val status: String //
 )
